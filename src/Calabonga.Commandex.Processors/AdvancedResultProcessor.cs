8d4c8bfa870bad0870c0c92b520da305b96d1947
Sergei Calabonga <s.calabonga@qwell.ru>
2024-10-09 07:17:28 +0300

    Done beta-1

---
+++ src/Calabonga.Commandex.Processors/AdvancedResultProcessor.cs
@@ -0,0 +1,32 @@
+﻿using Calabonga.Commandex.Engine.Base;
+using Calabonga.Commandex.Engine.Processors.Base;
+using Microsoft.Extensions.Logging;
+
+namespace Calabonga.Commandex.Engine.Processors;
+
+/// <summary>
+/// Implementation for <see cref="IResultProcessor"/> with some advanced features for result processing
+/// </summary>
+public class AdvancedResultProcessor : IResultProcessor
+{
+    private readonly IProcessor _processor;
+    private readonly ILogger<AdvancedResultProcessor> _logger;
+
+    public AdvancedResultProcessor(IProcessor processor, ILogger<AdvancedResultProcessor> logger)
+    {
+        _processor = processor;
+        _logger = logger;
+    }
+
+    public void ProcessCommand(ICommandexCommand command)
+    {
+        if (command.GetResult() is not IProcessorResult result)
+        {
+            _logger.LogInformation("Commandex command type {CommandType} processing skipped.", command.TypeName);
+            return;
+        }
+
+        result.Accept(_processor);
+        _logger.LogInformation("Commandex command type {CommandType} processing completed.", command.TypeName);
+    }
+}
