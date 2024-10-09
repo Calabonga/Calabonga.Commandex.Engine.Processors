fb8a6bd2f913fc172ea07ce4190dea8cbdefcaed
Sergei Calabonga <s.calabonga@qwell.ru>
2024-10-09 07:17:03 +0300

    results creates

---
+++ src/Calabonga.Commandex.Processors/Results/ClipboardResult.cs
@@ -0,0 +1,14 @@
+﻿using Calabonga.Commandex.Engine.Processors.Base;
+
+namespace Calabonga.Commandex.Engine.Processors.Results;
+
+public class ClipboardResult : ProcessorResult
+{
+    public string ClipboardData { get; }
+
+    public ClipboardResult(string clipboardData)
+        => ClipboardData = clipboardData;
+
+    public override void Accept(IProcessor processor)
+        => processor.Visit(this);
+}
---
+++ src/Calabonga.Commandex.Processors/Results/TextFileResult.cs
@@ -0,0 +1,22 @@
+﻿using Calabonga.Commandex.Engine.Processors.Base;
+
+namespace Calabonga.Commandex.Engine.Processors.Results;
+
+/// <summary>
+/// Text file processor for commandex result
+/// </summary>
+public sealed class TextFileResult : ProcessorResult, IProcessorResult
+{
+    public TextFileResult(string fileName, string text)
+    {
+        FileName = fileName.EndsWith(".txt") ? fileName : $"{FileName}.txt";
+        Text = text;
+    }
+
+    public string FileName { get; }
+
+    public string Text { get; }
+
+    public override void Accept(IProcessor processor)
+        => processor.Visit(this);
+}
