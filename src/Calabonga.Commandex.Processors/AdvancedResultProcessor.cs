c3e119bb805ef9453f467a3e845650a3bc962017
Sergei Calabonga <s.calabonga@qwell.ru>
2024-10-09 07:51:26 +0300

    Default result implemented when not any type of the result set up

--- src/Calabonga.Commandex.Processors/AdvancedResultProcessor.cs
+++ src/Calabonga.Commandex.Processors/AdvancedResultProcessor.cs
@@ -1,6 +1,8 @@
 ﻿using Calabonga.Commandex.Engine.Base;
+using Calabonga.Commandex.Engine.Dialogs;
 using Calabonga.Commandex.Engine.Processors.Base;
 using Microsoft.Extensions.Logging;
+using System.Text;
 
 namespace Calabonga.Commandex.Engine.Processors;
 
@@ -9,11 +11,16 @@ namespace Calabonga.Commandex.Engine.Processors;
 /// </summary>
 public class AdvancedResultProcessor : IResultProcessor
 {
+    private readonly IDialogService _dialogService;
     private readonly IProcessor _processor;
     private readonly ILogger<AdvancedResultProcessor> _logger;
 
-    public AdvancedResultProcessor(IProcessor processor, ILogger<AdvancedResultProcessor> logger)
+    public AdvancedResultProcessor(
+        IDialogService dialogService,
+        IProcessor processor,
+        ILogger<AdvancedResultProcessor> logger)
     {
+        _dialogService = dialogService;
         _processor = processor;
         _logger = logger;
     }
@@ -22,7 +29,34 @@ public class AdvancedResultProcessor : IResultProcessor
     {
         if (command.GetResult() is not IProcessorResult result)
         {
-            _logger.LogInformation("Commandex command type {CommandType} processing skipped.", command.TypeName);
+            _logger.LogInformation("Commandex command type {CommandType} processing skipped. Using a DefaultResultProcessor.", command.TypeName);
+
+            var pushState = command.IsPushToShellEnabled ? "Enabled" : "Disabled";
+            var stringBuilder = new StringBuilder($"{command.DisplayName} v.{command.Version}");
+            stringBuilder.AppendLine();
+            stringBuilder.AppendLine(command.Description);
+            stringBuilder.AppendLine();
+            stringBuilder.AppendLine($"{nameof(command.IsPushToShellEnabled)} is {pushState}");
+            stringBuilder.AppendLine();
+
+            if (command.IsPushToShellEnabled)
+            {
+                var res = command.GetResult();
+                if (res is null)
+                {
+                    stringBuilder.Append("Result is null.");
+                }
+                else
+                {
+                    stringBuilder.Append(res);
+                }
+            }
+
+            stringBuilder.AppendLine();
+            stringBuilder.AppendLine();
+            stringBuilder.AppendLine("Command execution successfully completed.");
+
+            _dialogService.ShowNotification(stringBuilder.ToString());
             return;
         }
 
