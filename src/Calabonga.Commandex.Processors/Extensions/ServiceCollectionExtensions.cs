e1049f0d496d0242f7f60f6ce4fd55882a9d7c29
Sergei Calabonga <s.calabonga@qwell.ru>
2024-10-09 07:16:13 +0300

    Extensions for ServiceCollection

---
+++ src/Calabonga.Commandex.Processors/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Calabonga.Commandex.Engine.Base;
+using Calabonga.Commandex.Engine.Processors.Base;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Calabonga.Commandex.Engine.Processors.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Register advanced result processor <see cref="IResultProcessor"/> for <see cref="ICommandexCommand"/> result post-processing.
+    /// </summary>
+    /// <param name="source"></param>
+    public static void AddAdvancedResultProcessor(this IServiceCollection source)
+    {
+        source.AddScoped<IProcessor, Processor>();
+        source.TryAddScoped<IResultProcessor, AdvancedResultProcessor>();
+    }
+}
