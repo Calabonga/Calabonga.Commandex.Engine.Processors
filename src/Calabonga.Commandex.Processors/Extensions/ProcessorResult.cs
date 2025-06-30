62c219406472aa7f3f9a9e07dd935ca858f3172d
Sergei Calabonga <s.calabonga@qwell.ru>
2025-06-30 13:21:14 +0700

    default processing changed

---
+++ src/Calabonga.Commandex.Processors/Extensions/ProcessorResult.cs
@@ -0,0 +1,15 @@
+﻿using Calabonga.Commandex.Engine.Processors.Base;
+
+namespace Calabonga.Commandex.Engine.Processors.Extensions;
+
+/// <summary>
+/// Processor result base class
+/// </summary>
+public abstract class ProcessorResult : IProcessorResult
+{
+    /// <summary>
+    /// Accepts as visitor <see cref="processor"/>
+    /// </summary>
+    /// <param name="processor"></param>
+    public abstract void Accept(IProcessor processor);
+}
